9c1778905bf66dbb65ddfaab2f5ed3ea94f4da09
Myokyeong Kim <77683645+data04190@users.noreply.github.com>
2022-05-10 00:15:06 +0900

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+from scipy import signal
+import mne
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+def lambda_handler(event, context):
+    print("test")
