017ee2da7593bef40b35f2f39e60238816264725
Myokyeong Kim <77683645+data04190@users.noreply.github.com>
2022-05-18 13:30:18 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -8,5 +8,14 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
+bucket = "kmk-practice" 
+file_name = "20211018_1_jhkwon_gum/S1.csv"
+
+s3 = boto3.client('s3')
+obj = s3.get_object(Bucket= bucket, Key= file_name)
+df = pd.read_csv(obj['Body'])
+print(df)
+
+
 def lambda_handler(event, context):
     print("test")
